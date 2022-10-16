a02986dd49b62e84316cae8eb93cdea9aaa411e2
北沢亮和 <ryowa.kitazawa@gmail.com>
2022-10-16 23:44:15 +0900

    途中まで

--- yujo_bank/apps.py
+++ yujo_bank/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class YujoBankConfig(AppConfig):
-    name = 'yujo_bank'
+    name = 'yujobank'
--- yujo_bank/views.py
+++ yujo_bank/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.utils import timezone
+from django.views.generic import TemplateView, CreateView, ListView, DetailView, UpdateView, DeleteView
+class IndexView(TemplateView):
+    template_name = 'index.html'
 
-# Create your views here.
+
+class DiaryCreateView(CreateView):
+    template_name = 'diary_create.html'
+    form_class = YujoBankForm
+    success_url = reverse_lazy('yujobank:diary_create_complete')
+
+
+class DiaryCreateCompleteView(TemplateView):
+    template_name = 'diary_create_complete.html'
+
+
+class DiaryListView(ListView):
+    template_name = 'diary_list.html'
+    model = YujoBank
+
+
+
+class DiaryDetailView(DetailView):
+    template_name = 'diary_detail.html'
+    model = YujoBank
+
+
+class DiaryUpdateView(UpdateView):
+    template_name = 'diary_update.html'
+    model = YujoBank
+    fields = ('date', 'title', 'text',)
+    success_url = reverse_lazy('yujobank:yujobank_list')
+
+    def form_valid(self, form):
+        YujoBank = form.save(commit=False)
+        YujoBank.updated_at = timezone.now()
+        YujoBank.save()
+        return super().form_valid(form)
+
+
+class DiaryDeleteView(DeleteView):
+    template_name = 'diary_detele.html'
+    model = YujoBank
+    success_url = reverse_lazy('yujobank:yujobank_list')
